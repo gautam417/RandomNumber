7715e73defc19cfc5ea11ac89afdf94035fd34f0
Gautam Mehta <gautammehta@gautams-macbook-air.local>
2018-07-21 18:16:17 -0700

    Initial Commit

---
+++ RandomNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomNumber
+//
+//  Created by Gautam Mehta on 7/21/18.
+//  Copyright © 2018 Gautam Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
